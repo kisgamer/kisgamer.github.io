7d9ff888888ad042ce388c58066f8f39a4a30149
kisgamer <64792441+kisgamer@users.noreply.github.com>
2021-02-27 10:02:41 +0100

    Add files via upload

---
+++ Skin Grabber Tool.py
@@ -0,0 +1,13 @@
+import webbrowser;
+
+print('This is using the Minotar API \nminotar.net\nMake sure to check out them!')
+name = input('Name A Minecraft Skin:\n')
+webbrowser.open("https://minotar.net/skin/" + name)
+webbrowser.open("https://minotar.net/armor/body/" + name + "/100.png")
+
+
+
+
+
+
+	
